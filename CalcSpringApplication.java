f32ad9b898b7dd757b0a8ad66d01c3462580ac97
VGlyva <135941893+VGlyva@users.noreply.github.com>
2023-11-26 14:28:31 +0700

    Мой первый коммит

--- CalcSpringApplication.java
+++ CalcSpringApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CalcSpringApplication {
+public class  CalcSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalcSpringApplication.class, args);
---
+++ controller/CalcController.java
@@ -0,0 +1,51 @@
+package pro.sky.CalcSpring.controller;
+
+import org.springframework.web.bind.annotation.*;
+import pro.sky.CalcSpring.service.CalcService;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RequestMapping("/calculator")
+@RestController
+public class CalcController {
+    private final CalcService calcService;
+
+    public CalcController(CalcService calcService) {
+        this.calcService = calcService;
+    }
+
+
+    @GetMapping
+    public String helloCalculator() {
+        return calcService.helloCalculator();
+    }
+
+    @GetMapping("/plus")
+    public String plusCalc(@RequestParam("num1") Integer num1,
+                           @RequestParam("num2") Integer num2) {
+        return num1 + " + " + num2 + " = " + calcService.plusCalc(num1, num2);
+    }
+
+    @GetMapping("/minus")
+    public String minusCalc(@RequestParam("num1") Integer num1,
+                            @RequestParam("num2") Integer num2) {
+        return num1 + " - " + num2 + " = " + calcService.minusCalc(num1, num2);
+    }
+
+    @GetMapping("/multiply")
+    public String multiply(@RequestParam("num1") Integer num1,
+                           @RequestParam("num2") Integer num2) {
+        return  num1 + " * " + num2 + " = " + calcService.multiply(num1, num2);
+    }
+
+    @GetMapping("/divide")
+    public String divide(@RequestParam("num1") Integer num1,
+                         @RequestParam("num2") Integer num2) {
+        return num1 + " / " + num2 + " = " + calcService.divide(num1, num2);
+    }
+    @ExceptionHandler(IllegalAccessException.class)
+    public String divideByZero(IllegalAccessException e, HttpServletResponse response) {
+        response.setStatus(400);
+        return e.getMessage();
+    }
+}
